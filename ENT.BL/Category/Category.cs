67442d2b5721c0fda6386a1dc91f3005ff89cd2b
mohsinalimomin <mohsinanvar123@gmail.com>
2025-05-27 10:20:58 +0530

    add scalaton of category

---
+++ ENT.BL/Category/Category.cs
@@ -0,0 +1,43 @@
+﻿using ENT.Model.Common;
+using ENT.Model.Category;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ENT.Model.Common;
+
+namespace ENT.BL.Category
+{
+    public class Category:ICategory
+    {
+        public Category() { }
+
+        public Task<APIResponseModel> Add(CategoryModel objCategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<APIResponseModel> Delete(int CategoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<APIResponseModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<APIResponseModel> GetById(int CategoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<APIResponseModel> Update(CategoryModel objCategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
