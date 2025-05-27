a5e5d28b0f630266088b22dde52b0478073a9539
Gami kiran <Gami kiran@DESKTOP-05KIL3J>
2025-05-27 10:30:31 +0530

    Add otp category

---
+++ ENT.BL/Otp/Otp.cs
@@ -0,0 +1,32 @@
+﻿using ENT.Model.Common;
+using ENT.Model.Otp;
+using ENT.Model.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENT.BL.Otp
+{
+    public class Otp : IOtp
+    {
+        public Otp()
+        {
+            
+        }
+        public async Task<APIResponseModel> Add(OtpModel objOtp)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<APIResponseModel> GetById(int OtpId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<APIResponseModel> Update(OtpModel objOtp)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebAPI/Controllers/Otp/OtpController.cs
@@ -0,0 +1,37 @@
+﻿using ENT.BL.Otp;
+using ENT.BL.User;
+using ENT.Model.Common;
+using ENT.Model.Otp;
+using ENT.Model.Users;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers.Otp
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OtpController : ControllerBase
+    {
+        private readonly IOtp _Otp;
+
+        public OtpController(IOtp otp)
+        {
+            _Otp = otp;
+        }
+        [HttpPost]
+        public async Task<APIResponseModel> Add(OtpModel objOtpModel)
+        {
+            return await _Otp.Add(objOtpModel);
+        }
+        [HttpPut]
+        public async Task<APIResponseModel> Update(OtpModel objOtpModel)
+        {
+            return await _Otp.Update(objOtpModel);
+        }
+        [HttpGet("GetById")]
+        public async Task<APIResponseModel> GetById(int OtpId)
+        {
+            return await _Otp.GetById(OtpId);
+        }
+    }
+}
