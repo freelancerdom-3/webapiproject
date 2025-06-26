9c41fee46f6a9da9a7b37702649316a079a2f82b
Gami kiran <Gami kiran@DESKTOP-05KIL3J>
2025-06-26 15:18:48 +0530

    Fees model change

--- ENT.Model/Fees/FeesModel.cs
+++ ENT.Model/Fees/FeesModel.cs
@@ -5,13 +5,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ENT.Model.Fees
 {
     public class FeesModel
     {
         [Key]
         public int FeesId { get; set; }
-        public string Range { get; set;}
-        public int Charge { get; set;}
+        public decimal StartRange { get; set; }
+        public decimal EndRange { get; set;}
+        public decimal Charge { get; set;}
+
     }
 }
